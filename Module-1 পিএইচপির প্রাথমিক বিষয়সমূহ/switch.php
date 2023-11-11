9cdaf2f4a917cc4ef3784d7096a162f117932643
rashidul191 <rashidul.191cse.gub@gmail.com>
2023-11-11 21:32:18 +0600

    in tutorial we learnd switch and case

---
+++ Module-1 পিএইচপির প্রাথমিক বিষয়সমূহ/switch.php
@@ -0,0 +1,36 @@
+<?php
+
+$num = 14;
+
+switch ($num % 2) {
+    case 0:
+        echo "{$num} is event number \n";
+        break;
+    case 11:
+        echo "{$num} is test \n";
+        break;
+    default:
+        echo "{$num} is odd number \n";
+}
+
+
+
+$color = "red";
+switch ($color) {
+
+    case "green":
+        echo "{$color} is our favorite color";
+        break;
+    case "yellow":
+        echo "{$color} is our favorite color";
+        break;
+    case "blue":
+        echo "{$color} is our favorite color";
+        break;
+    case "red":
+
+        echo "{$color} is our favorite color";
+        break;
+    default:
+        echo "{$color} is our favorite color";
+}
