6f5e2053e483421657479ca8ad2d8b6863f2017b
rashidul191 <rashidul.191cse.gub@gmail.com>
2023-11-12 22:09:25 +0600

    to day we learnd extra ternary operator

--- Module-1 পিএইচপির প্রাথমিক বিষয়সমূহ/ternaryOperator.php
+++ Module-1 পিএইচপির প্রাথমিক বিষয়সমূহ/ternaryOperator.php
@@ -1,20 +1,21 @@
 <?php
 
-// ternary Operator
+// Ternary Operator
 
-$num = 13;
-
+$num = 10;
 // normal condition operator like if else.
 if ($num % 2 === 0) {
-    echo "{$num} is event number";
+    echo "{$num} is event number.";
 } else {
-    
-    echo "{$num} is odd number";
+    echo "{$num} is odd number.";
 }
-
 echo "\n";
+
 // ternary operator
 echo $num % 2 === 0 ? "{$num} is event number" : "{$num} is odd number";
+echo "\n";
+// talk to extra ternary operator
 
-
-
+$num = 10;
+$result = ($num % 2 === 0) ? "A" : (($num === 11) ? "B" : "C");
+echo $result;
