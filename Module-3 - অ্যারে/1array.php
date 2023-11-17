c98d478b683317b59e9aece43a9147adbed9a3d1
rashidul191 <rashidul.191cse.gub@gmail.com>
2023-11-17 11:34:12 +0600

    php array

---
+++ Module-3 - অ্যারে/1array.php
@@ -0,0 +1,23 @@
+<?php
+// array of php
+
+// both array defined is same.
+// $numbers = array(1,2,3,4,5);
+$numbers = [1, 2, 3, 4, 5];
+
+// array length count();
+
+echo count($numbers) . "\n";
+
+// specific elements find from array
+echo $numbers[2] . "\n \n";
+
+$arrayLength = count($numbers);
+for ($i = 0; $i < $arrayLength; $i++) {
+
+    echo $numbers[$i] . "\n";
+}
+
+for ($i = $arrayLength-1; $i >= 0; $i--) {
+    echo $numbers[$i] . "\n";
+}
