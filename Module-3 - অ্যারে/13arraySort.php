6dfa46595592faa81faa4824cc6ce7e31bd3338c
rashidul191 <rashidul.191cse.gub@gmail.com>
2023-11-21 22:42:28 +0600

    in this tutorial i learnd array sort(), asrot(), ksort(), rsort(), and etc.

---
+++ Module-3 - অ্যারে/13arraySort.php
@@ -0,0 +1,26 @@
+<?php
+// array sort
+
+
+// small to big
+$rashidulFriends = array("samrat", "farhan", "noman", "abir", "hasan");
+print_r($rashidulFriends);
+sort($rashidulFriends);
+print_r($rashidulFriends);
+
+$names = array("s" => "samrat", "f" => "farhan", "n" => "noman", "abir", "hasan");
+print_r($names);
+asort($names);
+print_r($names);
+
+
+// big to small
+$numbers = array(1, 44, 55, 2, 3, 7);
+// arsort($numbers);
+print_r($numbers);
+
+foreach ($numbers as $key => $number) {
+
+    echo "\n" . $key;
+    // echo "\n" . $number;
+}
