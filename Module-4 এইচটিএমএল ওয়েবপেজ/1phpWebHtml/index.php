485ef66222fefe2187bff0b167337cb76517a209
rashidul191 <rashidul.191cse.gub@gmail.com>
2023-12-30 19:20:25 +0600

    in this tutorial i learnd how to setup php with html to run web page

---
+++ Module-4 এইচটিএমএল ওয়েবপেজ/1phpWebHtml/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Welcome To PHP WebSite using HTML</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, magnam!</p>
+    <h2>Rashidul Islam</h2>
+    <div>
+        <h2>Card-1</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, nobis.</p>
+    </div>
+
+</body>
+
+</html>
