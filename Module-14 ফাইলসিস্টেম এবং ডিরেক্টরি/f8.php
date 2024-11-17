5d7cf9c40ec0c8cc3e0a097bd6cd29ff1d73df6c
rashidul191 <rashidul.191cse.gub@gmail.com>
2024-11-17 23:09:07 +0600

    directory and file copy using php

--- Module-14 ফাইলসিস্টেম এবং ডিরেক্টরি/f8.php
+++ Module-14 ফাইলসিস্টেম এবং ডিরেক্টরি/f8.php
@@ -1,3 +1,36 @@
 <?php
 
-// Directory Move Or Copy
+// Directory With Files Copy Using PHP
+
+function copyDir($sourceFile, $destinationFile)
+{
+    if (!is_dir($sourceFile)) {
+        echo "Source directory does not exits.";
+        return;
+    }
+
+    if (!is_dir($destinationFile)) {
+        mkdir($destinationFile, 0777, true);
+    }
+
+
+    $files = scandir($sourceFile);
+
+    foreach ($files as $file) {
+        if ("." !== $file && ".." !== $file) {
+
+            $sourcePath = $sourceFile . "/" . $file;
+            $destinationPath = $destinationFile . "/" . $file;
+            if (is_dir($sourcePath)) {
+                copyDir($sourcePath, $destinationPath);
+            } else {
+                copy($sourcePath, $destinationPath);
+            }
+        }
+    }
+}
+
+$source = getcwd() . "/source/test";
+$destination = getcwd() . "/target/test";
+
+copyDir($source, $destination);
