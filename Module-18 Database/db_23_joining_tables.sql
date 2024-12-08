18607c14b1bc651cdac5dfa6e7cb9a18bd053920
rashidul191 <rashidul.191cse.gub@gmail.com>
2024-12-08 23:39:13 +0600

    date and time

--- Module-18 Database/db_23_joining_tables.sql
+++ Module-18 Database/db_23_joining_tables.sql
@@ -1,20 +1,23 @@
 -- Joining Tables
+-- Join
+SELECT table_name.column_name1, table_name.column_name2, table_name.column_name3, table_name.column_name4
+FROM
+    table_name,
+    other_table_name
+    JOIN table_name.Roll = other_table_name.Roll;
 
-SELECT
-    column_name1,
-    column_name2,
-    column_name3,
-    column_name4
-FROM table_name, other_table_name
-INNER JOIN table_name.Roll = other_table_name.Roll;
+SELECT student2_details.Roll, Reg_Number, Name, Gender, GPA
+FROM
+    student2_details
+    JOIN exam_result ON student2_details.Roll = exam_result.Roll;
+-- Inner Join
+SELECT table_name.column_name1, table_name.column_name2, table_name.column_name3, table_name.column_name4
+FROM
+    table_name,
+    other_table_name
+    INNER JOIN table_name.Roll = other_table_name.Roll;
 
-
-SELECT 
-    student2_details.Roll, 
-    Reg_Number, 
-    Name, 
-    Gender, 
-    GPA
-FROM student2_details
-INNER JOIN exam_result 
-    ON student2_details.Roll = exam_result.Roll;
+SELECT student2_details.Roll, Reg_Number, Name, Gender, GPA
+FROM
+    student2_details
+    INNER JOIN exam_result ON student2_details.Roll = exam_result.Roll;
